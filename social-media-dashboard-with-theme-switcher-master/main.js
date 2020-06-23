08e772df372b234c989a5dc710f1dfd995e58814
Kushank singh <singh.kushank12@gmail.com>
2020-06-23 23:49:24 +0530

    update

---
+++ social-media-dashboard-with-theme-switcher-master/main.js
@@ -0,0 +1,10 @@
+const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'light');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }
+}
+toggleSwitch.addEventListener('change', switchTheme, false);
